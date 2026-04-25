0ce2933a367fb107efcc8c220fff3f1f87b809f8
Jordi <jdiazb7527@gmail.com>
2026-04-25 13:43:29 +0200

    Solucinado problema de 404

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mi-portfolio">
       <App />  
     </BrowserRouter>
   </StrictMode>,
